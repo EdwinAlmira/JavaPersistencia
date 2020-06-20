1e632b85966634e02f3af55b24f04c9ef31daad7
EdwinAlmira <ealopezalmira@gmail.com>
2020-06-20 02:12:12 -0600

    Hecho el insert

--- meensajesApp/src/main/java/MensajesDAO.java
+++ meensajesApp/src/main/java/MensajesDAO.java
@@ -1,3 +1,10 @@
+
+import com.mysql.cj.Messages;
+import com.mysql.cj.xdevapi.PreparableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,8 +17,28 @@
  */
 public class MensajesDAO {
     
-    public void crearMensajeDB(Mensajes mensajes){
+    static Conexion dbConnect = new Conexion();
+    
+    public static void crearMensajesDB(Mensajes mensajes){
         
+        
+        try(Connection conx = dbConnect.getConnection()) {
+            
+            PreparedStatement ps = null;
+            try {
+                String query="INSERT INTO mensajes(mensaje, autor) value (?,?)";
+                ps=conx.prepareStatement(query);
+                ps.setString(1,mensajes.getMensaje());
+                ps.setString(2, mensajes.getAutorMensaje());
+                ps.executeUpdate();
+                System.out.println("El mensajes creado efectivamente");
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
     
     public static void leerMensajesDB(){
@@ -25,5 +52,4 @@ public class MensajesDAO {
     public static void actualizarMensajesDB(Mensajes mensajes){
         
     }
-    
 }
--- meensajesApp/src/main/java/MensajesService.java
+++ meensajesApp/src/main/java/MensajesService.java
@@ -1,3 +1,10 @@
+
+import com.google.protobuf.Internal;
+import com.google.protobuf.TextFormat;
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,7 +18,16 @@
 public class MensajesService {
 
     public static void crearMensajes(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nEscribe tu mensaje");
+        String mensaje = sc.nextLine();
+        System.out.println("\nEscribe tu nombre");
+        String autor = sc.nextLine();
         
+        Mensajes mensajes = new Mensajes();
+        mensajes.setMensaje(mensaje);
+        mensajes.setAutorMensaje(autor);
+        MensajesDAO.crearMensajesDB(mensajes);
     }
     
     public static void listarMensajes(){
